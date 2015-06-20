faa28a21f56adc662d983f81b6ed656313fcb549
Larry <lcconan@gmail.com>
2015-06-20 19:41:45 +0800

    added time delay function and an http header
    added time delay function and an http header

--- getZip.py
+++ getZip.py
@@ -1,8 +1,12 @@
 #reg = r'href="(.\.zip)"'
 import re
 import urllib
+import time
+import urllib2
 
-def getHtml(url):
+
+def getHtml(url,headers):
+    r = urllib2.Request(url,headers)
     page = urllib.urlopen(url)
     html = page.read()
     return html
@@ -14,13 +18,17 @@ def getZip(html):
     ziplist = re.findall(Zipre,html)
     return ziplist        
 
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+    }	
 qianyao = 'http://www.xxxzj.com/rom/'
 f=open('out.txt','w')
-for number in range(500, 999+1):
+for number in range(3201, 4499+1):
 	theExactUrl = qianyao + str(number) + '.htm'
-	html = getHtml(theExactUrl)
+	html = getHtml(theExactUrl,headers)
 	print getZip(html)
 	print>>f,getZip(html)
+	time.sleep(1)
 	
 
 
